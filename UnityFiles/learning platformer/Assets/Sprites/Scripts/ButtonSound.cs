6d11f5570ac858058e5fecb37213778cad6e731b
andre-Hazim <andre.hazim@ocsbstudent.ca>
2018-01-23 17:38:38 -0500

    Final game

---
+++ UnityFiles/learning platformer/Assets/Sprites/Scripts/ButtonSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+    public class ButtonSound : MonoBehaviour
+{
+    public AudioClip boop; 
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = boop;
+        source.playOnAwake = false;
+        button.onClick.AddListener(() => PlaySound());
+	}
+	
+	void PlaySound()
+    {
+        source.PlayOneShot(boop);
+    }
+}
---
+++ UnityFiles/learning platformer/Assets/Sprites/Scripts/backgroundsound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// creates background music for the game
+public class backgroundsound : MonoBehaviour
+{
+    private static backgroundsound instance = null;
+    private static backgroundsound Instance
+    {
+        get { return instance; }
+    }
+// allows it so that the music can continue through each scene
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+        }
+    }
